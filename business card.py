681bfb9c19a89595db994f839784a12163169198
itaislasky <72315767+itaislasky@users.noreply.github.com>
2020-10-03 20:47:34 +0300

    Add files via upload

---
+++ business card.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def ex1():    
+    print("*************************")
+    print("Itai Slasky")
+    print("Dereh Avot")
+    print("jr. Software Developer")
+    print("*************************")
+ex1()
